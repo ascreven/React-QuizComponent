f9fbe5b78f59b9c955601fdb479b3d70d3f0fec0
Alexandra Screven <alexandra.k.screven@accenturefederal.com>
2019-05-26 15:03:43 -0400

    created quiz component

---
+++ src/Quiz.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+let quizData = require('./quiz_data.json')
+
+class Quiz extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            quizPosition: 1
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="quizQuestion">
+                    {quizData.quiz_questions[0].instruction_text}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Quiz;
