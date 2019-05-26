fdccf314ed727b85561a371b1ba7fc127ce68877
Alexandra Screven <alexandra.k.screven@accenturefederal.com>
2019-05-26 16:06:20 -0400

    user can reset quiz at end

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,6 +15,12 @@ class Quiz extends Component {
         }
     }
 
+    handleResetClick() {
+        this.setState({
+            quiz_position: 1
+        })
+    }
+
     showNextQuestion() {
         this.setState({
             quiz_position: this.state.quiz_position + 1
@@ -26,7 +32,9 @@ class Quiz extends Component {
         return (
             <div>
                 { isQuizEnd
-                    ? <QuizEnd />
+                    ? <QuizEnd 
+                        resetClickHandler={this.handleResetClick.bind(this)}
+                        />
                     : <QuizQuestion 
                         showNextQuestionHandler={this.showNextQuestion.bind(this)}
                         quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
